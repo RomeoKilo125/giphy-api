6d49cd3ca654cdda87c48fec1109ff1cdddeb5a4
RomeoKilo125 <romeokilo@icloud.com>
2018-04-16 00:05:41 -0400

    returning results

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,63 @@
 app = function () {
-  let buttons = ["Phineas and Ferb", "Transformers", "Thundercats", "The Jetsons", "Looney Tunes", "Duck Tales", "Talespin", "Darkwing Duck", "Gargoyles", "Rocko's Modern Life"]
+  let buttons = ["Phineas and Ferb", "Transformers", "The Jetsons", "Looney Tunes", "Duck Tales", "Talespin", "Darkwing Duck", "Gargoyles", "Rocko's Modern Life"]
 
   showButtons = function() {
     $('#buttonGroup').empty()
     buttons.forEach(function(element) {
       let b = $('<button>')
       b.text(element);
-      b.data("value", element);
+      b.addClass("movieButton");
+      b.attr("data-value", element.replace(' ','+'));
+      console.log(element.replace(/ /g,'+'))
       $('#buttonGroup').append(b);
     });
   }
+
+  showGif = function(result) {
+    console.log(result);
+    let gif = $('<div>');
+    gif.attr('class', 'gif');
+    let p = $('<p>').text("Rating: " + result.rating);
+    let img = $('<img>');
+    img.attr('stillUrl', result.images.fixed_width_still.url);
+    img.attr('animatedUrl', result.images.fixed_width.url);
+    img.attr('src', result.images.fixed_width_still.url);
+    img.attr('state', "still");
+    gif.append(p);
+    gif.append(img);
+    $('#gifArea').append(gif);
+  }
+
+  $('#submit').on("click", function(e) {
+    e.preventDefault();
+    buttons.push($('#searchBar').val());
+    $('#searchBar').val('');
+    showButtons();
+  });
+
+  $(document).on("click", ".movieButton", function(e) {
+    e.preventDefault();
+    let movieName = $(this).data("value");
+    let apiKey = "ixY18TyZhxMDm0VMgOEFZJrUtEBOV4l6";
+
+    let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + movieName + '&api_key=' + apiKey + '&limit=12';
+
+    console.log(queryURL);
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      $('#gifArea').empty();
+      response.data.forEach(function(element) {
+        showGif(element);
+      });
+    }).fail(function() {
+      console.log("API Call Failed");
+    });
+
+  });
+
 
   showButtons();
 }
